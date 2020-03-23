8ff1ad110ad4f33246b3412558b4395909e3e5c4
songfd2018 <38712635+songfd2018@users.noreply.github.com>
2020-03-24 00:31:45 +0800

    Delete run_simulation_EPSR.sh

--- MainSimulation/ConvergenceDiagnostic/run_simulation_EPSR.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd MainSimulation/ConvergenceDiagnostic
-
-# run another BUSseq MCMC chain
-../../../../BUSseq-1.0/BUSseq -d./ -r../RawCountData/ -p simulation -v 1 -K 5 -i 4000 -o 1000 -s 2187 -c 8
-../../../../BUSseq-1.0/BUSseq_inference -d./ -r../RawCountData/ -p simulation -v 1 -K 5 -i 4000 -b 2000 -c 8
-
-# Calculate EPSR
-R --vanilla --slave < EPSR_analysis.R
