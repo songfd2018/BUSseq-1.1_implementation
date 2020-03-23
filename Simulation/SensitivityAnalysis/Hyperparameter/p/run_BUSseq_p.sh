3430a9104f4a378e2c1c1e6efad1e47a97faf2e9
songfd2018 <38712635+songfd2018@users.noreply.github.com>
2020-03-24 00:56:26 +0800

    Add files via upload

---
+++ Simulation/SensitivityAnalysis/Hyperparameter/p/run_BUSseq_p.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+cd SensitivityAnalysis/Hyperparameter/p
+
+# compile the source code
+make
+
+# Sensitivity Analysis for p
+
+para=p
+
+for index in 1 2 3 4
+do
+
+wkdir="$para$index"
+echo $wkdir
+
+if [ ! -d "$wkdir" ]
+then
+    echo "Folder doesn't exist. Creating now"
+    mkdir $wkdir
+    echo "Folder created"
+fi
+
+BUSseq_"$wkdir" -d./$wkdir/  -r../RawCountData/ -p simulation -v 1 -K 5 -i 4000 -o 1000 -s 9123 -c 8
+BUSseq_inference -d./$wkdir/  -r../RawCountData/ -p simulation -v 1 -K 5 -i 4000 -b 2000 -c 8
+
+done
+
+# Collect the ARI
+R-351 --vanilla --slave < "Collect_inference_$para.R"
